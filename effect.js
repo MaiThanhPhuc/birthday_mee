327ed552d97a3ac831d096a2c91f504df0437c39
MaiThanhPhuc <maithanhphuc7721@gmail.com>
2021-11-03 17:43:30 +0700

    upload

---
+++ effect.js
@@ -0,0 +1,258 @@
+$(window).load(function () {
+  $(".loading").fadeOut("fast");
+  $(".container").fadeIn("fast");
+});
+$("document").ready(function () {
+  var vw;
+  $(window).resize(function () {
+    vw = $(window).width() / 2;
+  });
+
+  $("#turn_on").click(function () {
+    var audio = $(".song")[0];
+    audio.play();
+    $("body").addClass("peach");
+    $(".bannar").addClass("bannar-come");
+    $(".cake").fadeIn("slow");
+    $(".balloons").addClass("hotairballoon");
+    $(this)
+      .fadeOut("slow")
+      .delay(4000)
+      .promise()
+      .done(function () {
+        $("#light_candle").fadeIn("slow");
+      });
+  });
+
+  $("#light_candle").click(function () {
+    $(".fuego").fadeIn("slow");
+    $(this)
+      .fadeOut("slow")
+      .delay(3000)
+      .promise()
+      .done(function () {
+        $("#story").fadeIn("slow");
+      });
+  });
+
+  $("#story").click(function () {
+    $(this).fadeOut("fast");
+    $(".cake")
+      .fadeOut("fast")
+      .promise()
+      .done(function () {
+        animationTimeline();
+      });
+  });
+});
+
+//alert('hello');
+// msg
+
+// Animation Timeline
+const animationTimeline = () => {
+  const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
+  const hbd = document.getElementsByClassName("wish-hbd")[0];
+
+  textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
+    .split("")
+    .join("</span><span>")}</span`;
+
+  hbd.innerHTML = `<span>${hbd.innerHTML
+    .split("")
+    .join("</span><span>")}</span`;
+
+  const ideaTextTrans = {
+    opacity: 0,
+    y: -20,
+    rotationX: 5,
+    skewX: "15deg",
+  };
+
+  const ideaTextTransLeave = {
+    opacity: 0,
+    y: 20,
+    rotationY: 5,
+    skewX: "-15deg",
+  };
+
+  const tl = new TimelineMax();
+
+  tl.to(".message", 0.1, {
+    visibility: "visible",
+  })
+    .from(".three", 0.7, {
+      opacity: 0,
+      y: 10,
+      // scale: 0.7
+    })
+    .to(
+      ".three",
+      0.7,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      "+=2"
+    )
+    .from(".four", 0.7, {
+      scale: 0.2,
+      opacity: 0,
+    })
+    .from(".fake-btn", 0.3, {
+      scale: 0.2,
+      opacity: 0,
+    })
+    .staggerTo(
+      ".hbd-chatbox span",
+      0.5,
+      {
+        visibility: "visible",
+      },
+      0.05
+    )
+    .to(".fake-btn", 0.1, {
+      backgroundColor: "#f2985c",
+    })
+    .to(
+      ".four",
+      0.5,
+      {
+        scale: 0.2,
+        opacity: 0,
+        y: -150,
+      },
+      "+=0.7"
+    )
+    .from(".idea-1", 0.7, ideaTextTrans)
+    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-2", 0.7, ideaTextTrans)
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 0.7, ideaTextTrans)
+    .to(".idea-3 strong", 0.5, {
+      scale: 1.2,
+      x: 10,
+      backgroundColor: "rgb(21, 161, 237)",
+      color: "#fff",
+    })
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-4", 0.7, ideaTextTrans)
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(
+      ".idea-5",
+      0.7,
+      {
+        rotationX: 15,
+        rotationZ: -10,
+        skewY: "-5deg",
+        y: 50,
+        z: 10,
+        opacity: 0,
+      },
+      "+=0.5"
+    )
+    .to(
+      ".idea-5 span",
+      0.7,
+      {
+        rotation: 90,
+        x: 8,
+      },
+      "+=0.4"
+    )
+    .to(
+      ".idea-5",
+      0.7,
+      {
+        scale: 0.2,
+        opacity: 0,
+      },
+      "+=2"
+    )
+    .from(
+      ".lydia-dp",
+      0.5,
+      {
+        scale: 3.5,
+        opacity: 0,
+        x: 25,
+        y: -25,
+        rotationZ: -45,
+      },
+      "-=2"
+    )
+    .from(".hat", 0.5, {
+      x: -100,
+      y: 350,
+      rotation: -180,
+      opacity: 0,
+    })
+    .staggerFrom(
+      ".wish-hbd span",
+      0.7,
+      {
+        opacity: 0,
+        y: -50,
+        // scale: 0.3,
+        rotation: 150,
+        skewX: "30deg",
+        ease: Elastic.easeOut.config(1, 0.5),
+      },
+      0.1
+    )
+    .staggerFromTo(
+      ".wish-hbd span",
+      0.7,
+      {
+        scale: 1.4,
+        rotationY: 150,
+      },
+      {
+        scale: 1,
+        rotationY: 0,
+        color: "#6999ff",
+        ease: Expo.easeOut,
+      },
+      0.1,
+      "party"
+    )
+    .from(
+      ".wish h5",
+      0.5,
+      {
+        opacity: 0,
+        y: 10,
+        skewX: "-12deg",
+      },
+      "party"
+    )
+    .staggerTo(
+      ".eight svg",
+      10,
+      {
+        visibility: "visible",
+        opacity: 0,
+        scale: 80,
+        repeat: 3,
+        repeatDelay: 1.4,
+      },
+      0.3
+    )
+    .to(".six", 1, {
+      opacity: 0,
+      zIndex: "-1",
+    })
+    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    .to(
+      ".last-smile",
+      0.5,
+      {
+        rotation: 90,
+      },
+      "+=1"
+    );
+  const replyBtn = document.getElementById("replay");
+  replyBtn.addEventListener("click", () => {
+    tl.restart();
+  });
+};
